5087ce1259ad6aee1a8b00722a6d85d0bf2e5ad3
Scott Liu <blindlf@hotmail.com>
2016-03-28 15:14:36 +0800

    Functional

--- src/bg.js
+++ src/bg.js
@@ -1,15 +1,31 @@
 chrome.webRequest.onCompleted.addListener(
 	function(details) {
-		chrome.tabs.sendMessage(details.tabId, {qstLoaded: true});
-	}, {urls: ['*://m.ajax.taobao.com/qst.htm*', '*://gm.mmstat.com/tbrate*']}
+		chrome.tabs.sendMessage(details.tabId, {qst: details.url});
+	}, {urls: ['*://m.ajax.taobao.com/qst.htm*']}
+);
+
+chrome.webRequest.onCompleted.addListener(
+	function(details) {
+		if (details.url.indexOf('rep=1')>=0) {
+			return;
+		}
+		chrome.tabs.sendMessage(details.tabId, {captcha: details.url});
+	}, {urls: ['*://img1.tbcdn.cn/tfscom/TB1*']}
 );
 
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		// read `newIconPath` from request and read `tab.id` from sender
-		chrome.browserAction.setIcon({
-			path: request.newIconPath,
-			tabId: sender.tab.id
-		});
+		if (request.iconPath) {
+			chrome.browserAction.setIcon({
+				path: request.iconPath,
+				tabId: sender.tab.id
+			});
+		} else if (request.badgeText) {
+			chrome.browserAction.setBadgeText({
+				text: request.badgeText,
+				tabId: sender.tab.id
+			});
+		}
 	}
 );
--- src/contentscript.js
+++ src/contentscript.js
@@ -1,11 +1,3 @@
-chrome.runtime.onMessage.addListener(
-	function(request, sender) {
-		if (request.qstLoaded) {
-			// TODO
-		}
-	}
-);
-
 function ready(fn) {
 	if (document.readyState != 'loading'){
 		fn();
@@ -14,16 +6,153 @@ function ready(fn) {
 	}
 }
 
-var dateStart;
+// Change icon and text
+function setIcon(path) {
+	chrome.runtime.sendMessage({'iconPath' : path});
+}
+function setBadgeText(text) {
+	chrome.runtime.sendMessage({'badgeText' : text});
+}
 
-function monitor() {
-	var secs = date - new Date() / 1000;
-	// Expired
-	if (secs < 0) {
+// Status enum
+var STATUS = {
+	NotAvailable: 0, // 仅限其他客户端
+	RequireLogin: 1, // 未登录
+	NotReady: 2, // 未开始
+	Ready: 3, // 刷新抢宝
+	Question: 4, // 回答问题
+	Expired: 5 // 已结束
+};
+
+function getStatus() {
+	if (null != document.querySelector('#J_SecKill .upper-begun')) {
+		// class .not-ready also presence
+		return STATUS.NotAvailable;
+	}
+	if (null != document.querySelector('#J_SecKill .need-login')) {
+		return STATUS.RequireLogin;
+	}
+	if (null != document.querySelector('#J_SecKill .not-ready')) {
+		return STATUS.NotReady;
+	}
+	if (null != document.querySelector('#J_SecKill .J_RefreshStatus')) {
+		return STATUS.Ready;
+	}
+	if (null != document.querySelector('#J_SecKill img.question-img')) {
+		return STATUS.Question;
+	}
+	if (null != document.querySelector('#J_SecKill .no-stock')) {
+		return STATUS.Expired;
+	}
+}
+
+function getStartDate() {
+	var edate = document.querySelector('#J_SecKill span.date');
+	var etime = document.querySelector('#J_SecKill span.time');
+	if (!edate || !etime) {
 		return;
 	}
-	chrome.runtime.sendMessage({'newIconPath' : 'icon-19.png'});
-	// Not ready
+	var time = edate.textContent
+		.replace('年', '-').replace('月', '-').replace('日', '');
+	var time = time + ' ' + etime.textContent;
+	return new Date(time);
+}
+
+function getEtaSecs() {
+	var date = getStartDate();
+	if (!date) {
+		return;
+	}
+	return (date - new Date()) / 1000;
+}
+
+// Wait to start before 5 minutes
+function reloadPage() {
+	setTimeout(function() {
+		console.log("eta", getEtaSecs());
+		if (getEtaSecs() < 300) {
+			window.location.reload();
+		} else {
+			reloadPage();
+		}
+	}, 5000);
+	setIcon('icon-19.png');
+	setBadgeText('W');
+}
+
+// 刷新抢宝
+function refresh() {
+	console.log('refresh', getEtaSecs());
+	setIcon('icon-19.png');
+	if (getEtaSecs() <= 3.0) {
+		refreshStick();
+	} else {
+		setBadgeText('R');
+		setTimeout(function() {
+			refresh();
+		}, 2500);
+	}
+}
+
+function refreshStick() {
+	var btn = document.querySelector('#J_SecKill .J_RefreshStatus');
+	if (!btn) {
+		return;
+	}
+	var event = document.createEvent('HTMLEvents');
+	event.initEvent('click', true, false);
+	// *://img1.tbcdn.cn/tfscom/TB1* should be requested
+	btn.dispatchEvent(event);
+	setBadgeText('H');
+	console.log('refreshStick', getEtaSecs());
+}
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender) {
+		// Refresh after 0.2 second
+		if (request.qst && STATUS.Ready === getStatus()) {
+			setTimeout(refreshStick, 200);
+		}
+		// Captcha loaded
+		if (request.captcha) {
+			processCaptcha();
+		}
+	}
+);
+
+function processCaptcha() {
+	// Process captcha
+	var eimg = document.querySelector('#J_SecKill img.question-img');
+	if (!eimg) {
+		return;
+	}
+	// Already processed
+	if (document.querySelector('#J_SecKill canvas')) {
+		return;
+	}
+	setBadgeText('A');
+
+	// Draw
+	var img = new Image();
+	img.crossOrigin = '';
+	img.onload = function() {
+		var canvas = document.createElement('canvas');
+		canvas.setAttribute('width', eimg.clientWidth);
+		canvas.setAttribute('height', eimg.clientHeight);
+		eimg.parentNode.appendChild(canvas);
+		var ctx = canvas.getContext('2d');
+		ctx.drawImage(img, 0, 0);
+
+		Caman(canvas, function () {
+			this.threshold(90).render();
+		});
+	};
+	img.src = eimg.src + '?rep=1';
+
+	var inp = document.querySelector('#J_SecKill input.answer-input');
+	if (inp) {
+		inp.focus();
+	}
 }
 
 // Get the start of time
@@ -33,28 +162,14 @@ ready(function() {
 		return;
 	}
 	container.addEventListener('DOMSubtreeModified', function () {
-		// Already monitor
-		if (dateStart) {
-			return;
-		}
-		// Require login
-		if (null != this.querySelector('.need-login')) {
-			return;
-		}
-		// Parse start time
-		if (null != this.querySelector('.not-ready')) {
-			var eDate = this.querySelector('span.date');
-			var eTime = this.querySelector('span.time');
-			// Something wrong
-			if (null == eDate || null == eTime) {
-				return;
-			}
-			// prase
-			var sTime = eDate.textContent + ' ' + eTime.textContent;
-			sTime = sTime.replace('年', '-').replace('月', '-').replace('日', '');
-			dateStart = new Date(sTime);
-			
-			monitor();
+		var status = getStatus();
+		console.log("status", status);
+		if (STATUS.NotReady === status) {
+			reloadPage();
+		} else if (STATUS.Ready === status) {
+			refresh();
+		} else if (STATUS.Question === status) {
+			//processCaptcha();
 		}
 	}, false);
 });
